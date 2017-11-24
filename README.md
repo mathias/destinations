e6d79b62897a525c851d4d2223d500965887da62
Matt Gauger <matt.gauger@gmail.com>
2017-11-24 11:09:49 -0600

    Actually generate some stories, as simple as they may be

--- README.md
+++ README.md
@@ -8,6 +8,20 @@ An entry to the 2017 NaNoGenMo. A novel about all the people passing through LAX
 
 I begin by making a new Ruby project. At the top of the script, I'm copying in the [list of airports from the LAX website](http://www.lawa.org/LAXAirlines.aspx) and formatting it into Ruby arrays so that I can use that later as a data source. I also note the abbreviation 'TBIT' everywhere, apparently that stands for 'Tom Bradley International Terminal' so I decide that I'll have an array of expansions that I can apply randomly to expand acronyms.
 
+### 2017/11/24 9:15AM:
+
+Added in some more data, including the total number of passengers for domestic and international flights in September 2017, so that I can calculate percentages that a passenger is on an international or domestic flight and procedurally generate. (based on http://www.lawa.org/uploadedfiles/LAX/statistics/tcom-0917.pdf )
+
+I also grabbed a list of departure cities for LAX from Wikpedia (https://en.wikipedia.org/wiki/Los_Angeles_International_Airport#Airlines_and_destinations) and spent way too much time formatting the table into Ruby arrays and strings.
+
+# 2017/11/24 11:00AM:
+
+Started classes for both domestic and international passengers and started adding attributes to them. I also started outputting sample stories even though I think ultimately, I'll have it generate all the attributes and then pick randomly from several different story templates for each type (domestic or international)
+
+There's quite a few more details I want to procedurally generate yet, including whether the person is traveling alone or not. I also want it to decide if they have a layover (or time until their flight) and whether the person goes and does something: goes shopping (and what they buy or don't buy), eat at a restaurant, get coffee, read, sleep, etc.
+
+My data didn't work out perfectly -- I really should have separated DESTINATIONS into DOMESTIC_DESTINATIONS and INTERNATIONAL_DESTINATIONS, so that I could avoid having people "traveling domestically going to Dubai". But the data from Wikipedia wasn't split up this way, and I don't want to do all the manual work to separate out the domestic destinations from the list.
+
 ## License:
 
 Copyright (c) 2013 Matt Gauger
--- write_story.rb
+++ write_story.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'faker'
 
+# From http://www.lawa.org/LAXAirlines.aspx
 AIRLINES = [
   ['Aer Lingus', 'EI', 'Terminal 2'],
   ['Aeroflot', 'SU', 'TBIT'],
@@ -83,6 +84,279 @@ AIRLINES = [
   ['XL Airways France', 'SE', 'Terminal 6']
 ]
 
-EXPANSIONS [
+EXPANSIONS = [
   ['TBIT', 'Tom Bradley International Terminal']
 ]
+
+# From http://www.lawa.org/uploadedfiles/LAX/statistics/tcom-0917.pdf
+#PASSENGER_TRAFFIC_TOTALS_SEPT_2017 = [
+  #{ type: "Domestic", count: 4739872 },
+  #{ type: "International", count: 2045217 }
+#]
+# The above can be simplified as: type/total = chance_<type>
+TOTAL_FLIGHTS = 4739872.0 + 2045217.0
+CHANCE_DOMESTIC = 4739872 / TOTAL_FLIGHTS
+CHANCE_INTERNATIONAL = 2045217 / TOTAL_FLIGHTS
+
+# From https://www.lawa.org/uploadedFiles/OurLAX/pdf/LAX_Survey_Final_Draft_REPORT_2012_08_19.pdf
+CHANCE_CONNECTING_FLIGHT = 0.38
+CHANCE_ORIGINATING_FROM_LAX = 0.62
+
+# From http://www.lawa.org/uploadedfiles/LAX/statistics/CY2017%20LAX%20On-Airport%20RAC%20Monthly%20Adjusted%20Gross%20Revenue%20and%20Transactions.pdf
+LAX_RENTAL_CARS_PER_YEAR = 273881.0
+
+LOCAL_TRANSPORTATION = [
+  'LAX Shuttle',
+  'FlyAway Bus',
+  'Hotel Shuttle',
+  'Shared Ride Van',
+  'City Bus',
+  'Rental car',
+  'Taxi',
+  'Lyft',
+  'Uber',
+  'friend\'s car',
+  'limo'
+]
+
+# From https://en.wikipedia.org/wiki/Los_Angeles_International_Airport
+LAX_HUB_FOR = [
+  'Alaska Airlines',
+  'American Airlines',
+  'Delta Air Lines',
+  'United Air Lines'
+]
+
+DESTINATIONS = [
+  ['Aer Lingus', ['Dublin']],
+  ['Aeroflot', ['Moscow–Sheremetyevo']],
+  ['Aeroméxico', ['Guadalajara', 'Mexico City']],
+  ['Aeroméxico Connect', ['León/Del Bajío', 'Monterrey']],
+  ['Air Canada', ['Calgary', 'Montréal–Trudeau', 'Toronto–Pearson', 'Vancouver']],
+  ['Air China', ['Beijing–Capital']],
+  ['Air France', ['Papeete', 'Paris–Charles de Gaulle']],
+  ['Air New Zealand', ['Auckland', 'London–Heathrow', 'Rarotonga']],
+  ['Air Tahiti Nui', ['Papeete', 'Paris–Charles de Gaulle']],
+  ['Alaska Airlines', ['Anchorage', 'Baltimore', 'Guadalajara', 'Havana', 'Ixtapa/Zihuatanejo', 'Liberia', 'Loreto', 'Manzanillo', 'Mazatlán', 'Mexico City', 'Portland, Oregon', 'Puerto Vallarta', 'Salt Lake City', 'San José del Cabo', 'San José de Costa Rica', 'Seattle/Tacoma', 'Washington–National']],
+  ['Alaska Airlines', ['Mammoth Lakes', 'Medford', 'San Jose, California', 'Santa Rosa', 'Sun Valley', 'Mexico City']],
+  ['Alitalia', ['Rome–Fiumicino']],
+  ['All Nippon Airways', ['Tokyo–Haneda', 'Tokyo–Narita']],
+  ['Allegiant Air', ['Albuquerque', 'Bellingham', 'Boise', 'Cincinnati', 'Eugene', 'Grand Junction', 'McAllen', 'Medford', 'Memphis', 'Ogden', 'Provo', 'Reno/Tahoe', 'Springfield, Missouri', 'Tulsa', 'Billings', 'Cedar Rapids/Iowa City', 'Des Moines', 'Fargo', 'Little Rock', 'Missoula', 'Montrose', 'Oklahoma City', 'Sioux Falls', 'Wichita']],
+  ['American Airlines', ['Atlanta', 'Austin', 'Beijing–Capital', 'Belize City', 'Boston', 'Cancún', 'Charlotte', 'Chicago–O\'Hare', 'Columbus–Glenn', 'Dallas/Fort Worth', 'Denver', 'Hartford', 'Havana', 'Hong Kong', 'Honolulu', 'Indianapolis', 'Kahului', 'Kailua–Kona', 'Las Vegas', 'Lihue', 'London–Heathrow', 'Mexico City', 'Miami', 'Nashville', 'New Orleans', 'New York City', 'Orlando', 'Philadelphia', 'Phoenix', 'Puerto Vallarta', 'Raleigh/Durham', 'St. Louis', 'San José del Cabo', 'São Paulo–Guarulhos', 'Seattle/Tacoma', 'Shanghai–Pudong', 'Sydney', 'Tokyo–Haneda', 'Tokyo–Narita', 'Toronto–Pearson', 'Vancouver', 'Washington–Dulles', 'Washington DC', 'Anchorage', 'Auckland', 'Vail', 'Sacramento']],
+  ['American Eagle', ['Albuquerque', 'Denver', 'El Paso', 'Eugene', 'Fayetteville/Bentonville', 'Fresno', 'Houston–Intercontinental', 'Kansas City', 'Mazatlán', 'Medford', 'Montrose', 'Oklahoma City', 'Omaha', 'Phoenix', 'Portland, Oregon', 'Reno/Tahoe', 'Sacramento', 'Salt Lake City', 'San Antonio', 'San Diego', 'San Francisco', 'San Jose, California', 'Seattle/Tacoma', 'Tucson', 'Vancouver', 'Aspen', 'Austin', 'Grand Junction', 'Jackson Hole', 'Redmond/Bend']],
+  ['Asiana Airlines', ['Seoul–Incheon']],
+  ['Austrian Airlines', ['Vienna']],
+  ['Avianca', ['Bogotá']],
+  ['Avianca Costa Rica', ['Guatemala City', 'San José de Costa Rica', 'San Salvador']],
+  ['Avianca El Salvador', ['San Salvador']],
+  ['Boutique Air', ['Inyokern', 'Merced']],
+  ['British Airways', ['London–Heathrow']],
+  ['Cathay Pacific', ['Hong Kong']],
+  ['China Airlines', ['Taipei–Taoyuan']],
+  ['China Eastern Airlines', ['Nanjing', 'Shanghai–Pudong']],
+  ['China Southern Airlines', ['Guangzhou']],
+  ['Copa Airlines', ['Panama City']],
+  ['Delta Air Lines', ['Atlanta', 'Austin', 'Belize City', 'Boston', 'Cancún', 'Cincinnati', 'Columbus–Glenn', 'Dallas/Fort Worth', 'Denver', 'Detroit', 'Guatemala City', 'Honolulu', 'Indianapolis', 'Ixtapa/Zihuatanejo', 'Kahului', 'Kailua–Kona', 'Kansas City', 'Las Vegas', 'Lihue', 'Memphis', 'Mexico City', 'Minneapolis/St. Paul', 'Nashville', 'New Orleans', 'New York City', 'Oakland', 'Orlando', 'Phoenix', 'Portland, Oregon', 'Puerto Vallarta', 'Raleigh/Durham', 'Sacramento', 'Salt Lake City', 'San Diego', 'San Jose, California', 'San José del Cabo', 'San José de Costa Rica', 'San Salvador', 'Shanghai–Pudong', 'Sydney', 'Tampa', 'Tokyo–Haneda', 'Washington DC', 'Liberia (CR)', 'Managua', 'Miami', 'Vancouver']],
+  ['Delta Connection', ['Albuquerque', 'Austin', 'Boise', 'Dallas/Fort Worth', 'Kansas City', 'Las Vegas', 'Oakland', 'Phoenix', 'Sacramento', 'San Antonio', 'San Diego', 'San Jose, California', 'Spokane', 'Tucson', 'Vancouver', 'Aspen', 'Bozeman', 'Denver', 'Jackson Hole', 'Kalispell', 'Missoula', 'Portland, Oregon', 'Sun Valley']],
+  ['Delta Shuttle', ['San Francisco', 'Seattle/Tacoma']],
+  ['El Al', ['Tel Aviv–Ben Gurion']],
+  ['Emirates', ['Dubai–International']],
+  ['Ethiopian Airlines', ['Addis Ababa', 'Dublin']],
+  ['Etihad Airways', ['Abu Dhabi']],
+  ['EVA Air', ['Taipei–Taoyuan']],
+  ['Fiji Airways', ['Nadi']],
+  ['Frontier Airlines', ['Atlanta', 'Chicago–O\'Hare', 'Denver', 'Orlando', 'Cincinnati', 'Colorado Springs']],
+  ['Great Lakes Airlines', ['Prescott']],
+  ['Hainan Airlines', ['Changsha, Chengdu', 'Chongqing']],
+  ['Hawaiian Airlines', ['Honolulu', 'Kahului', 'Lihue', 'Kailua–Kona']],
+  ['Hong Kong Airlines', ['Hong Kong']],
+  ['Iberia', ['Madrid']],
+  ['Interjet', ['Cancún', 'Guadalajara', 'León/Del Bajío', 'Mexico City', 'Puerto Vallarta', 'San José del Cabo']],
+  ['Japan Airlines', ['Osaka–Kansai', 'Tokyo–Narita']],
+  ['JetBlue Airways', ['Boston', 'Buffalo', 'Fort Lauderdale', 'Orlando', 'New York City']],
+  ['KLM', ['Amsterdam']],
+  ['Korean Air', ['Seoul–Incheon']],
+  ['LATAM Chile', ['Lima', 'Santiago de Chile']],
+  ['LATAM Perú', ['Lima']],
+  ['Level', ['Barcelona']],
+  ['LOT Polish Airlines', ['Warsaw–Chopin']],
+  ['Lufthansa', ['Frankfurt', 'Munich']],
+  ['Mokulele Airlines', ['El Centro']],
+  ['Norwegian Air Shuttle', ['Barcelona', 'Copenhagen', 'London', 'Oslo–Gardermoen', 'Paris–Charles de Gaulle', 'Rome–Fiumicino', 'Stockholm–Arlanda']],
+  ['Philippine Airlines', ['Manila']],
+  ['Qantas', ['Brisbane', 'Melbourne–Tullamarine', 'Sydney']],
+  ['Qatar Airways', ['Doha']],
+  ['Saudia', ['Jeddah', 'Riyadh']],
+  ['Scandinavian Airlines', ['Stockholm–Arlanda']],
+  ['Sichuan Airlines', ['Chengdu', 'Hangzhou', 'Jinan']],
+  ['Singapore Airlines', ['Seoul–Incheon', 'Singapore', 'Tokyo–Narita']],
+  ['Southwest Airlines', ['Albuquerque', 'Atlanta', 'Austin', 'Baltimore', 'Cancún', 'Chicago–Midway', 'Dallas', 'Denver', 'El Paso', 'Houston', 'Indianapolis', 'Kansas City', 'Las Vegas', 'Milwaukee', 'Nashville', 'New Orleans', 'Oakland', 'Phoenix', 'Pittsburgh', 'Portland, Oregon', 'Puerto Vallarta', 'Reno/Tahoe', 'Sacramento', 'St. Louis', 'Salt Lake City', 'San Antonio', 'San Francisco', 'San Jose, California', 'San José del Cabo', 'Tucson', 'Omaha']],
+  ['Spirit Airlines', ['Atlanta', 'Baltimore', 'Cleveland', 'Chicago–O\'Hare', 'Dallas/Fort Worth', 'Denver', 'Detroit', 'Fort Lauderdale', 'Houston–Intercontinental', 'Kansas City', 'Las Vegas', 'Minneapolis/St. Paul', 'New Orleans', 'Oakland', 'Philadelphia', 'Pittsburgh']],
+  ['Sun Country Airlines', ['Minneapolis/St. Paul']],
+  ['Swiss International Air Lines', ['Zürich']],
+  ['Thomas Cook Airlines', ['Manchester (UK)']],
+  ['Turkish Airlines', ['Istanbul–Atatürk']],
+  ['United Airlines', ['Boston', 'Cancún', 'Chicago–O\'Hare', 'Cleveland', 'Denver', 'Hilo', 'Honolulu', 'Houston–Intercontinental', 'Kahului', 'Kailua–Kona', 'Las Vegas', 'Lihue', 'London–Heathrow', 'Melbourne', 'Mexico City', 'Newark', 'Orlando', 'San Diego', 'San Francisco', 'Shanghai–Pudong', 'Singapore', 'Sydney', 'Tokyo–Narita', 'Washington–Dulles', 'Austin', 'Baltimore', 'Puerto Vallarta', 'San José del Cabo', 'Seattle/Tacoma']],
+  ['United Express', ['Albuquerque', 'Austin', 'Boise', 'Colorado Springs', 'Dallas/Fort Worth', 'Fresno', 'Las Vegas', 'León/Del Bajío', 'Minneapolis/St. Paul', 'Monterey', 'Oklahoma City', 'Palm Springs', 'Phoenix', 'Redmond',  'Reno/Tahoe', 'Sacramento', 'Salt Lake City', 'San Antonio', 'San Diego', 'San Francisco', 'San Luis Obispo', 'Santa Barbara', 'Seattle/Tacoma', 'St. George, Utah', 'Tucson', 'Vancouver', 'Aspen', 'Bozeman', 'Hayden/Steamboat Springs', 'Jackson Hole', 'Kalispell', 'Missoula', 'Montrose']],
+  ['Virgin America', ['Boston', 'Cancún', 'Chicago–O\'Hare', 'Dallas–Love', 'Fort Lauderdale', 'Honolulu', 'Kahului', 'Las Vegas', 'New York City', 'Newark', 'Orlando', 'Philadelphia', 'Portland, Oregon', 'San Francisco', 'Seattle/Tacoma', 'Washington DC']],
+  ['Virgin Atlantic', ['London–Heathrow']],
+  ['Virgin Australia', ['Brisbane', 'Melbourne–Tullamarine', 'Sydney']],
+  ['VivaAerobus', ['Guadalajara']],
+  ['Volaris', ['Acapulco', 'Aguascalientes', 'Durango', 'Guadalajara', 'León/Del Bajío', 'Mexico City', 'Morelia', 'Oaxaca', 'Queretaro', 'Uruapan', 'Zacatecas', 'Puerto Vallarta']],
+  ['WestJet', ['Calgary', 'Edmonton', 'Toronto–Pearson', 'Vancouver']],
+  ['WOW air', ['Reykjavík–Keflavík']],
+  ['XiamenAir', ['Qingdao', 'Xiamen']],
+  ['XL Airways France', ['Paris–Charles de Gaulle']]
+]
+
+class Passenger
+  attr_accessor :name, :originating_from_lax, :gender, :connecting, :gender, :mood, :trip_purpose, :leaving, :arriving
+
+  def initialize
+    @name = Faker::Name.name
+    @connecting = rand < CHANCE_ORIGINATING_FROM_LAX
+    if !@connecting
+      @leaving = rand < 0.50
+      @arriving = !leaving
+    else
+      @arriving = true
+    end
+
+    @gender = generate_gender
+    @trip_purpose = generate_trip_purpose
+    @mood = generate_mood
+  end
+
+  def connecting?
+    connecting
+  end
+
+  # https://data.worldbank.org/indicator/SP.POP.TOTL.FE.ZS
+  def generate_gender
+    case rand
+    when 0..0.49
+      ["she", "her", "hers"]
+    when 0.50..0.99
+      ["he", "him", "his"]
+    else
+      ["they", "them", "theirs"]
+    end
+  end
+
+  def capitalized_they
+    gender[0].capitalize
+  end
+
+  def generate_mood
+    case trip_purpose[0]
+    when :vacation
+      ['excited', 'nervous', 'tired', 'sunburned'].sample
+    when :business
+      ['tired', 'overworked', 'nervous'].sample
+    when :funeral
+      ['sad', 'tired', 'grief-stricken', 'content'].sample
+    else
+      ['excited', 'nervous', 'tired', 'sad', 'angry', 'anxious', 'ecstatic', 'delirious'].sample
+    end
+  end
+
+  def generate_trip_purpose
+    case rand
+    when 0..0.33
+      [:business, 'for business']
+    when 0.34..0.49
+      [:vacation, 'on a vacation']
+    when 0.50..0.74
+      [:family, 'to visit family']
+    when 0.75..0.85
+      [:pilgrimage, 'on a pilgrimage']
+    when 0.86..0.90
+      [:funeral, 'to attend a funeral']
+    when 0.91..0.5
+      [:wedding, 'to attend a wedding']
+    else
+      [:secret, 'on top-secret business']
+    end
+  end
+end
+
+class DomesticPassenger < Passenger
+  attr_reader :from_state, :destination, :arriving_from
+
+  def initialize
+    super
+    @from_state = Faker::Address.state
+
+    arrival = DESTINATIONS.sample
+    @arrival_airline = arrival[0]
+    @arriving_from = arrival[1].sample
+
+    departure = DESTINATIONS.sample
+    @going_to_airline = departure[0]
+    @destination = departure[1].sample
+  end
+
+  def run
+    output = "#{name} is a domestic passenger. #{gender[0].capitalize} is from #{from_state}."
+    if connecting?
+      output += " #{capitalized_they} is connecting through LAX, coming from #{arriving_from} and headed to #{destination}."
+      output += " #{name} is flying on #{@arrival_airline}."
+    else
+      output += " #{capitalized_they} is leaving from LAX to go to #{destination}."
+    end
+    output += " #{name} is traveling #{trip_purpose[1]}." if rand > 0.3
+    output += " #{capitalized_they} are #{mood}." if rand > 0.3
+    output
+  end
+end
+
+class InternationalPassenger < Passenger
+  attr_reader :country_of_origin, :arrival_airline, :arriving_from
+
+  def initialize
+    super
+    @country_of_origin = Faker::Address.country
+
+    arrival = DESTINATIONS.sample
+    @arrival_airline = arrival[0]
+    @arriving_from = arrival[1].sample
+
+    complex_flight = rand < 0.15 # 15% chance of flying on separate airlines
+    if complex_flight
+      departure = DESTINATIONS.sample
+      @going_to_airline = departure[0]
+      @going_to_city = departure[1].sample
+    else
+      @going_to_airline = arrival[0]
+      @going_to_city = arrival[1].sample
+    end
+  end
+
+  def run
+    output = "#{@name} is an international passenger."
+    output += " #{capitalized_they} is a citizen of #{@country_of_origin}."
+
+    if connecting?
+      output += " Arriving from #{@arriving_from} on #{@arrival_airline}. Going to #{@going_to_city} on #{@going_to_airline}."
+    else
+      output += " #{capitalized_they} is leaving from LAX to travel to #{@going_to_city} on #{@going_to_airline}."
+    end
+    output
+  end
+end
+
+# Begin generating passengers:
+# First of all, is this passenger a domestic or international passenger?
+
+def generate_passenger_stories
+  10.times do |i|
+    domestic_or_international = rand
+    if domestic_or_international < CHANCE_DOMESTIC
+      puts DomesticPassenger.new.run
+    else
+      puts InternationalPassenger.new.run
+    end
+  end
+end
+
+generate_passenger_stories
